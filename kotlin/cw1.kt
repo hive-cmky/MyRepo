d4bab7a1bf081bf31bca56542f75fb65e9012aa9
abhijit.maharik16@gmail.com <abhijit.maharik16@gmail.com>
2023-09-04 21:20:53 +0530

    first kotlin file added

--- kotlin/cw1.kt
+++ kotlin/cw1.kt
@@ -1,3 +1,21 @@
+/*Welcome to your first journey on learning Kotlin!
+
+What will you learn here?
+
+    By the end of this course, you will be comfortable with all the basic features of Kotlin, and will be able to write simple code.
+    The focus is to get you to a level where you can attempt easy CodeChef programming problems.
+
+Let the fun begin!
+Click on Next to start. */
+
+/*Let us output an integer in Kotlin.
+
+The main function is the entry point of every Kotlin program.
+The print() function is what we use to output/print anything to the screen. We will learn about functions in the coming modules, just remember print() function is use to output anything on the screen.
+
+This code just prints 12 on the screen when you run it.
+Click on Submit to know the result. */
+
 fun main(args : Array<String>){
     print(12)
 }
