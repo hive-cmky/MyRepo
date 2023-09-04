289c96eb47594562cae7155f1dd18e611ecf32cc
abhijit.maharik16@gmail.com <abhijit.maharik16@gmail.com>
2023-09-04 21:14:43 +0530

    printing text Kotlin file added

---
+++ kotlin/cw2.kt
@@ -0,0 +1,17 @@
+/*
+Printing text
+In the last problem, we saw how to output an integer (using print()).
+Now let's see how to output some text.
+There is only one difference. All textual content should start and end with double quotes.
+For eg -
+
+print("Hey")
+
+Task
+
+    Replace the _ (underscores) in the editor with the text "I love Kotlin" to output "I love Kotlin".
+ */
+
+ fun main(args : Array<String>){
+    print("I love Kotlin")
+ }
