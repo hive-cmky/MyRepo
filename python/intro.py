21929ebae93aa16e9cb4e90318c5c0ba55b543fc
abhijit.maharik16@gmail.com <abhijit.maharik16@gmail.com>
2023-09-05 08:01:11 +0530

    my first python file added

---
+++ python/intro.py
@@ -0,0 +1,12 @@
+# Read the code below. Click on 'Submit' to check the output and continue.
+
+
+# Welcome to your first journey on learning Python!
+
+
+# Let the fun begin!
+# Click on Next to start.
+
+print(12)
+
+print("I love Python!")
